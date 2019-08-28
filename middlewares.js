d277aacaeb6aeb81903094fce329c61cb77b1185
Guillaume Esnault <mon-compte1131@live.fr>
2019-08-28 16:58:50 +0200

    style: formatting text

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,7 @@
 const Sentry = require('@sentry/node');
 
-export const onError = (err, req, res, next) => {// The error id is attached to `res.sentry` to be returned
+export const onError = (err, req, res, next) => {
+    // The error id is attached to `res.sentry` to be returned
     // and optionally displayed to the user for support.
     res.statusCode = 500;
     console.log(`SENTRY ERROR N°${res.sentry} : `, err);
